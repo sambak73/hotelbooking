c26ef594c5de6e1d50672a1afff889c940f8dadb
sambak73 <raghuisnow@yahoo.com>
2023-01-19 19:22:45 +0530

    Updated Home.py to read data.csv file and generate content in 2 columns

--- arditsulce-1/app2/Home.py
+++ arditsulce-1/app2/Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 #st.set_page_config(layout="wide")
 
@@ -21,3 +22,27 @@ with col2:
     Following are list of applications created using python. There is also link to source code.
     """
 st.write(content2)
+#st.image("images/20.png")
+with open("data.csv", "r") as file:
+    data = file.readlines()
+col3, col4 = st.columns(2)
+for index, title in enumerate(data):
+    listitem = title.split(";")
+    #st.write(f"{index}.{listitem[1]}")
+    #st.write(f"{index} . {title}")
+
+    if index > 0:
+        image = listitem[3].strip('\n')
+        image = Image.open(f"images/{image}")
+        if index % 2 > 0:
+            with col3:
+                st.title(listitem[0])
+                st.write(listitem[1])
+                st.image(image)
+                st.info(listitem[2])
+        if index % 2 == 0:
+            with col4:
+                st.title(listitem[0])
+                st.write(listitem[1])
+                st.image(image)
+                st.info(listitem[2])
