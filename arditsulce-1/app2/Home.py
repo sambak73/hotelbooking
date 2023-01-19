29ba98ceced9bba8658f26fe6f0697cf234635f6
sambak73 <raghuisnow@yahoo.com>
2023-01-19 17:02:28 +0530

    Updated image in the page

--- arditsulce-1/app2/Home.py
+++ arditsulce-1/app2/Home.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 st.title("My Portfolio")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpeg")
+    st.image("images/Nature-PNG-Image.png")
 with col2:
     content = """
     This is the profile page of myself.
@@ -15,6 +15,7 @@ with col2:
     The page is introduction of myself and my interests in the field of Information Technology
     This is just a start to the complete profile page
     """
+    st.title("Samba Krishnamurthy")
     st.info(content)
     content2 = """
     Following are list of applications created using python. There is also link to source code.
