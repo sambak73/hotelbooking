436e6c40828f4c5b831997801328ae3d4a0c84df
sambak73 <raghuisnow@yahoo.com>
2023-01-21 04:51:56 +0530

    added page and footer

--- arditsulce-1/app4/app4.py
+++ arditsulce-1/app4/app4.py
@@ -10,9 +10,15 @@ for index, row in df.iterrows():
     pdf.add_page()
 
     pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(0, 0, 254)
     pdf.cell(w=0, h=12, txt=row["Topic"], border=0, align="L", ln=1)
     pdf.line(10,20,200,20)
 
+    pdf.ln(246)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(120, 120, 120)
+    pdf.cell(w=0, h=8, txt=row["Topic"], border=0, align="R")
 
-
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
 pdf.output("output.pdf")
