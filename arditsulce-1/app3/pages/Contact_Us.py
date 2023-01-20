41451451a3182554f2a8352248a40c8a8c318e87
sambak73 <raghuisnow@yahoo.com>
2023-01-20 12:01:00 +0530

    updated the app to send with topics

--- arditsulce-1/app3/pages/Contact_Us.py
+++ arditsulce-1/app3/pages/Contact_Us.py
@@ -1,11 +1,13 @@
+import pandas
 import streamlit as st
 import smtplib,ssl
+import pandas as pd
 host = "smtp.gmail.com"
 port = 465
 sender = "sambasivam.k@gmail.com"
 password = "xgbswpanmtgyevjf"
 receiver = "smbkrishnamurthy@gmail.com"
-
+data = pd.read_csv("topics.csv")
 context = ssl.create_default_context()
 st.title("Contact Us")
 def send_email(email,message):
@@ -14,12 +16,18 @@ def send_email(email,message):
         server.sendmail(email,receiver,message)
 
 with st.form(key="contact_form"):
+    topics = []
     email = st.text_input("Your Email","", key="email")
-    message = st.text_area("Your Message","", key="query")
+    for index, topic in data.iterrows():
+        topics.append(topic["topic"])
+    option = st.selectbox("Topic to discuss", topics)
+    message = st.text_area("Your Message", "", key="query")
     messages = f"""\
-    Subject: New email from {email}
-    {message}
-    """
+Subject: {option} by {email}
+
+From: {email}
+{message}
+"""
     submit = st.form_submit_button("Submit")
     if submit:
         send_email(email,messages)
