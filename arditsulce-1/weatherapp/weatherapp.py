aecaf64c9777d1b227ff6f55dacb3176a5174a91
sambak73 <raghuisnow@yahoo.com>
2023-01-30 13:49:07 +0530

    added plot and dummy data to connect UI selection with plot

--- arditsulce-1/weatherapp/weatherapp.py
+++ arditsulce-1/weatherapp/weatherapp.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
@@ -7,3 +8,15 @@ days = st.slider('Forecast Days:', min_value=1, max_value=5,
 option = st.selectbox('Select the data to view',('Temperature','Sky'))
 
 st.subheader(f'{option} for the next {days} days in {place}')
+
+def get_value(days):
+    dates = ['2022-12-02','2022-12-03','2022-12-04']
+    temp = [10.1,12.3,11.2]
+    temp_days = [days * i for i in temp]
+    return dates, temp_days
+
+d,t = get_value(days)
+
+fig = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature"})
+
+st.plotly_chart(fig)
